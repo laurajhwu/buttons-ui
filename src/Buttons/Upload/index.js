dcb16cf0095ffea035961b78049e0da15b798a2f
Laura <laura.jh.wu@gmail.com>
2021-07-17 13:00:38 +0800

    complete upload button functions

--- src/Buttons/Upload/index.js
+++ src/Buttons/Upload/index.js
@@ -1,17 +1,32 @@
 import React from "react";
-
+import uuid from "react-uuid";
 import { Button, Icon, Text, FileInput, Label } from "./styles";
 
 export default function Upload(props) {
-  const { disabled, text, iconEnd, iconStart, theme, type, onChange } = props;
+  const id = uuid();
+  const { disabled, text, iconEnd, iconStart, theme, onChange, accept } = props;
+  function handleOnChange(event) {
+    const file = event.target.files[0];
+
+    if (file) {
+      onChange && onChange(file);
+    } else {
+      throw "no file was uploaded";
+    }
+  }
 
   return (
     <Button theme={theme?.button} disabled={disabled}>
-      <Label htmlFor="upload-file" theme={theme?.label}>
+      <Label htmlFor={`upload-file${id}`} theme={theme?.label}>
         {iconStart && <Icon src={iconStart} theme={theme?.icon} />}
         <Text theme={theme?.text}>{text}</Text>
         {iconEnd && <Icon src={iconEnd} theme={theme?.icon} />}
-        <FileInput type="file" accept={type || "*"} id="upload-file" />
+        <FileInput
+          type="file"
+          accept={accept || "*"}
+          id={`upload-file${id}`}
+          onChange={handleOnChange}
+        />
       </Label>
     </Button>
   );
