10b6967ab3910f8db40cd881eda96bcb4415868d
Laura Wu <laura.jh.wu@gmail.com>
2021-07-17 21:24:42 +0800

    display select button usage instruction

--- src/Display/Select.js
+++ src/Display/Select.js
@@ -16,7 +16,7 @@ const theme = {
     //icon styling
   },
   text: {
-    //text styling
+    //button text styling
   },
   options: {
     //options styling
@@ -42,30 +42,38 @@ export default function UploadBtn() {
       title="Select Button"
       description={
         <>
-          Upload Button constructed from html <code>&lt;input&gt;</code> tag
-          that accepts all or some file types - please refer{" "}
-          <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#unique_file_type_specifiers">
-            here
-          </a>{" "}
-          for customization.
+          Select Button that allows full customization, along with the following
+          options and methods listed below.
           <br />
           <ul>
             <li>
-              <code>onChange</code>: returns <code>event.target.files[0]</code>{" "}
-              as an argument, if no file is found, then the function throws an
-              error and does not execute.
+              <code>onSelect</code>: takes a callback and returns the{" "}
+              <code>value</code> property from the selected option as an
+              argument.
             </li>
             <br />
             <li>
-              <code>icon</code>, <code>text</code>, and <code>input</code>{" "}
-              elements are wrapped within a <code>&lt;label&gt;</code> tag.{" "}
-              <code>input</code> display is set to <code>none</code> for styling
-              purposes.
+              <code>hideOnSelect</code>: default value is <code>true</code>.
+              Setting the <code>false</code> prevents the selection dropdown
+              menu from closing after an option is selected. (must click button
+              to close)
             </li>
             <br />
             <li>
-              <code>input</code> id is combined with a randomly generated unique
-              identifier for labeling precision.
+              <code>defaultValue</code>: takes the value of an option to be set
+              as default.
+            </li>
+            <br />
+            <li>
+              <code>options</code>: an array of options to be rendered. Each
+              option is an object that contains the <code>content</code>{" "}
+              property, which will be rendered on screen; <code>key</code>{" "}
+              property, the key for React to render arrays; <code>value</code>{" "}
+              property, which is optional, for the html <code>value</code>{" "}
+              property in html <code>option</code>.
+              <br />
+              If no <code>value</code> property is passed, the default{" "}
+              <code>value</code> will be the <code>content</code>.
             </li>
           </ul>
           <br />
@@ -75,37 +83,73 @@ export default function UploadBtn() {
         <code className="language-js">
           {`//Customized Styles
             const theme = {
+              \u00A0select: {
+                \u00A0\u00A0//select styles
+              \u00A0},
               \u00A0button: {
                 \u00A0\u00A0marginTop: "10px",
                 \u00A0\u00A0//button styles
               \u00A0},
-               \u00A0label: {
-                 \u00A0 \u00A0//label styling
-               \u00A0},
               \u00A0icon: {
                 \u00A0\u00A0//icon styling
               \u00A0},
               \u00A0text: {
-                \u00A0\u00A0//text styling
+                \u00A0\u00A0//button text styling
               \u00A0},
+              \u00A0options: {
+                \u00A0\u00A0//options styling
+                \u00A0\u00A0"& option": {
+                  \u00A0\u00A0\u00A0//individual option styling
+                \u00A0\u00A0},
+              \u00A0},
+               \u00A0arrow: {
+                 \u00A0 \u00A0//dropdown arrow styling
+               \u00A0},
             };
             ...
-            const [fileName, setFileName] = useState("");
-
-            function handleOnChange(file) {
-            \u00A0//file === event.target.files[0]
-            \u00A0setFileName(file.name);
-            }
+            const options = [
+              \u00A0{ content: "Language", key: "all", value: "select" /* optional */ },
+              \u00A0{ content: "English", key: "eng", value: "english" /* optional */ },
+              \u00A0{ content: "Chinese", key: "ch", value: "chinese" /* optional */ },
+              \u00A0{ content: "French", key: "fr", value: "french" /* optional */ },
+            ];
             ...
             //Render Button
             return (
-           \u00A0<Upload
-            \u00A0\u00A0text="Upload"
-            \u00A0\u00A0iconEnd={PlaceholderIcon}
-            \u00A0\u00A0theme={theme}
-            \u00A0\u00A0accept="image/*"
-            \u00A0\u00A0onChange={handleOnChange}
-            \u00A0/>
+              \u00A0<label className="demo-label">
+                \u00A0\u00A0Default Settings
+                \u00A0\u00A0<Select iconStart={PlaceholderIcon} theme={theme} options={options} />
+              \u00A0</label>
+
+              \u00A0<label className="demo-label">
+                \u00A0\u00A0No auto hide
+                \u00A0\u00A0<Select
+                  \u00A0\u00A0\u00A0iconStart={PlaceholderIcon}
+                  \u00A0\u00A0\u00A0theme={theme}
+                  \u00A0\u00A0\u00A0options={options}
+                  \u00A0\u00A0\u00A0hideOnSelect={false}
+                \u00A0\u00A0/>
+              \u00A0</label>
+
+              \u00A0<label className="demo-label">
+                \u00A0\u00A0Set default value
+                \u00A0\u00A0<Select
+                  \u00A0\u00A0\u00A0iconStart={PlaceholderIcon}
+                  \u00A0\u00A0\u00A0theme={theme}
+                  \u00A0\u00A0\u00A0options={options}
+                  \u00A0\u00A0\u00A0defaultValue="chinese"
+                \u00A0\u00A0/>
+              \u00A0</label>
+
+               \u00A0<label className="demo-label">
+                \u00A0\u00A0Disabled
+                \u00A0\u00A0<Select
+                  \u00A0\u00A0\u00A0iconStart={PlaceholderIcon}
+                  \u00A0\u00A0\u00A0theme={theme}
+                  \u00A0\u00A0\u00A0options={options}
+                  \u00A0\u00A0\u00A0disabled={true}
+                \u00A0\u00A0/>
+              \u00A0</label>
           );`}
         </code>
       )}
@@ -126,7 +170,7 @@ export default function UploadBtn() {
       <label className="demo-label">
         Set default value
         <Select
-          defaultValue={"chinese"}
+          defaultValue="chinese"
           iconStart={PlaceholderIcon}
           theme={theme}
           options={options}
@@ -135,7 +179,6 @@ export default function UploadBtn() {
       <label className="demo-label">
         Disabled
         <Select
-          text="Language"
           iconStart={PlaceholderIcon}
           disabled={true}
           theme={theme}
