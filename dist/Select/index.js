ef86dedc16dd15170e10f25e025c63fda208172c
Laura Wu <laura.jh.wu@gmail.com>
2021-07-18 15:40:56 +0800

    update dist folder

--- dist/Select/index.js
+++ dist/Select/index.js
@@ -15,9 +15,7 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-// import uuid from "react-uuid";
 function SelectBtn(props) {
-  // const id = uuid();
   const {
     disabled,
     defaultValue,
@@ -62,9 +60,6 @@ function SelectBtn(props) {
       setValue(((_children$find = children.find(option => defaultValue === option.props.value)) === null || _children$find === void 0 ? void 0 : _children$find.props.children) || children[0].props.children);
     }
 
-    if (children) {}
-
-    console.log(children);
     window.addEventListener("click", clickOutside);
     return () => {
       window.removeEventListener("click", clickOutside);
